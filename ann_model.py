0d9d6239e16bd93ceababfa4d9fc7b48aa964178
Prosper Arineitwe Asiimwe <prox0591@gmail.com>
2025-04-22 09:41:58 +0200

    Build ANN model inside ann_model.py

---
+++ ann_model.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+class FashionMNISTANN(nn.Module):
+    def __init__(self):
+        super(FashionMNISTANN, self).__init__()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(28 * 28, 128)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(128, 64)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.relu1(self.fc1(x))
+        x = self.relu2(self.fc2(x))
+        x = self.fc3(x)
+        return x
